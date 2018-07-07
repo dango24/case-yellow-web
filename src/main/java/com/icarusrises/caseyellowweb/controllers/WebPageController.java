1dcd3cb04b5bddc0107d447fb25230fa4189bcda
Oren Koriat <oren@cynerio.co>
2018-07-07 19:31:35 +0300

    added different index html pages for windows and linux web users

--- src/main/java/com/icarusrises/caseyellowweb/controllers/WebPageController.java
+++ src/main/java/com/icarusrises/caseyellowweb/controllers/WebPageController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,8 +16,14 @@ import javax.validation.Valid;
 @Controller
 public class WebPageController {
 
-    @RequestMapping("/home")
-    public String home() {
+    @RequestMapping({"/", "/home"})
+    public String home(@RequestHeader(value="User-Agent") String userAgent) {
+
+        System.out.println("USER AGENT: "+ userAgent);
+
+        if (userAgent.toLowerCase().contains("linux")) {
+            return "index_linux";
+        }
         return "index";
     }
 
